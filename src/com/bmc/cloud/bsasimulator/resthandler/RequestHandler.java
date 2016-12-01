4561fa0dba76b2f3bf6e4843be67140fb12c91bb
Pratik32 <pratikbhau92@gmail.com>
2016-12-01 23:53:41 +0530

    Made changes to RequestHandler

--- src/com/bmc/cloud/bsasimulator/resthandler/RequestHandler.java
+++ src/com/bmc/cloud/bsasimulator/resthandler/RequestHandler.java
@@ -17,31 +17,8 @@ public class RequestHandler implements HttpHandler {
 	public void handle(HttpExchange arg0) throws IOException {
 		// TODO Auto-generated method stub
 		
-		BSASimulator simulator=new BSASimulatorImpl();
-		Authenticator authenticator =  simulator.getAuthenticator();
-		BSAProcessor processor = simulator.getBSAProcessor();
-
-		URI uri = arg0.getRequestURI();
-		String path = uri.getPath();
-		String credentials = uri.getQuery();
-
-		String auth[] = credentials.split("&");
-		String user[] = auth[0].split("=");
-		String pass[] = auth[1].split("=");
-		
-		boolean valid = authenticator.authenticate(user[1], pass[1]);
-		String response=null;
-		if(valid){
-			System.out.println("Authentication Successful");
-		   // response = processor.process(path);
-		}
-		else{
-			System.out.println("Authentication Unsuccessful");
-			response = "Authentication Failed";
-	}
-		arg0.sendResponseHeaders(200, response.length());
-		OutputStream out = arg0.getResponseBody();
-		out.write(response.getBytes());
+		//Used JAX-RS annotations instead.
+		//Moved db handling to SQLiteDBHelper.
 		
 	}
 }
